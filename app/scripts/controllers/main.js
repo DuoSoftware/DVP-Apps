8211541375ae004373582e7214784cb133518a29
sukitha <sukithaj@gmail.com>
2015-12-09 10:15:33 +0530

    initial version

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dvpAppsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dvpAppsApp
+ */
+angular.module('dvpAppsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
