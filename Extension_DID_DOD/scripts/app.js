809d89a7aa81396ed0172445925a956cfb986a22
heshanindika <k.heshan.indika@gmail.com>
2016-01-05 10:34:02 +0530

    add DID dialogbox

--- Extension_DID_DOD/scripts/app.js
+++ Extension_DID_DOD/scripts/app.js
@@ -2,7 +2,7 @@
  * Created by Heshan.i on 12/30/2015.
  */
 (function(){
-  var app = angular.module("extension_DID_DOD",["ngRoute"]);
+  var app = angular.module("extension_DID_DOD",["ngRoute",'ngMaterial']);
   app.config(function($routeProvider){
     $routeProvider
       .when("/extension",{
--- Extension_DID_DOD/scripts/services/sipUserEndpointService.js
+++ Extension_DID_DOD/scripts/services/sipUserEndpointService.js
@@ -3,6 +3,8 @@
  */
 (function(){
   var sipUser = function($http){
+    var didNumber= null;
+    var extension= null;
     var getExtensions = function(accessToken){
       return $http.get("http://sipuserendpointservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/SipUser/Extensions/OfCompany/"+accessToken)
         .then(function(response){
@@ -20,9 +22,64 @@
           return response.data;
         });
     };
+    var addDidNumber = function(accessToken, didInfo){
+        return $http({
+          method: 'POST',
+          url: 'http://sipuserendpointservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/SipUser/DidNumber',
+          headers: {
+            'authorization': accessToken
+          },
+          data:didInfo
+        })
+          .then(function(response){
+            return response.data;
+          });
+    };
+    var updateDidWithExtension = function(accessToken, didNumber, extension){
+      return $http({
+        method: 'POST',
+        url: 'http://sipuserendpointservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/SipUser/DidNumber/'+didNumber+'/AssignToExt/'+extension,
+        headers: {
+          'authorization': accessToken
+        }
+      })
+        .then(function(response){
+          return response.data;
+        });
+    };
+    var updateDidState = function(accessToken, didNumber, state){
+      return $http({
+        method: 'POST',
+        url: 'http://sipuserendpointservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/SipUser/DidNumber/'+didNumber+'/Activate/'+state,
+        headers: {
+          'authorization': accessToken
+        }
+      })
+        .then(function(response){
+          return response.data;
+        });
+    };
+    var deleteDidNumber = function(accessToken,id){
+      return $http({
+        method: 'DELETE',
+        url: 'http://sipuserendpointservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/SipUser/DidNumber/'+id,
+        headers: {
+          'authorization': accessToken
+        }
+      })
+        .then(function(response){
+          return response.data;
+        });
+    }
     return{
       getExtensions:getExtensions,
-      getDidNumbers:getDidNumbers
+      getDidNumbers:getDidNumbers,
+      addDidNumber:addDidNumber,
+      updateDidWithExtension:updateDidWithExtension,
+      updateDidState:updateDidState,
+      deleteDidNumber:deleteDidNumber,
+      didNumber:didNumber,
+      extension:extension
     };
   };
   var module = angular.module("extension_DID_DOD");
