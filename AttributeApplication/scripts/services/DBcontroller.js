017bbd97e1a96f217d83e260bda2bd9cafbcd67f
sukitha <sukithaj@gmail.com>
2016-01-14 14:56:40 +0530

    conference added

--- AttributeApplication/scripts/services/DBcontroller.js
+++ AttributeApplication/scripts/services/DBcontroller.js
@@ -11,7 +11,7 @@
   var dbcontroller = function ($http,$mdDialog,$mdMedia) {
 
     var getAttributeList = function () {
-      return $http.get("http://localhost:8832/DVP/API/6.0/ResourceManager/Attributes")
+      return $http.get("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Attributes")
         .then(function (response) {
           return response;
         });
@@ -21,7 +21,7 @@
 
     var attribDelete = function (Attribute) {
 
-      return $http.delete("http://localhost:8832/DVP/API/6.0/ResourceManager/Attribute/"+Attribute.AttributeId)
+      return $http.delete("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Attribute/"+Attribute.AttributeId)
         .then(function (response) {
 
           response.AttributeId=Attribute.AttributeId;
@@ -43,7 +43,7 @@
 
 
       }
-      return $http.put("http://localhost:8832/DVP/API/6.0/ResourceManager/Attribute/"+edtObj.AttributeId,data)
+      return $http.put("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Attribute/"+edtObj.AttributeId,data)
         .then(function (response) {
 
           return response;
@@ -60,10 +60,8 @@
         AttType:"typ",
         AttCategory:"cat"
 
-
-
       }
-      return $http.post("http://localhost:8832/DVP/API/6.0/ResourceManager/Attribute",data)
+      return $http.post("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Attribute",data)
         .then(function (response) {
 
           console.log(response);
@@ -72,7 +70,7 @@
     }
 
     var getGroupList = function () {
-      return $http.get("http://localhost:8832/DVP/API/6.0/ResourceManager/Groups")
+      return $http.get("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Groups")
         .then(function (response) {
           return response;
         });
@@ -91,7 +89,7 @@
 
 
       }
-      return $http.put("http://localhost:8832/DVP/API/6.0/ResourceManager/Group/" + edtObj.GroupId, data)
+      return $http.put("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Group/" + edtObj.GroupId, data)
         .then(function (response) {
 
           return response;
@@ -100,7 +98,7 @@
 
     var groupDelete = function (group) {
 
-      return $http.delete("http://localhost:8832/DVP/API/6.0/ResourceManager/Group/"+group.GroupId)
+      return $http.delete("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Group/"+group.GroupId)
         .then(function (response) {
 
           response.GroupId= group.GroupId;
@@ -122,7 +120,7 @@
 
 
       }
-      return $http.post("http://localhost:8832/DVP/API/6.0/ResourceManager/Group",data)
+      return $http.post("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Group",data)
         .then(function (response) {
 
           return response;
@@ -137,7 +135,7 @@
 
 
       }
-      return $http.put("http://localhost:8832/DVP/API/6.0/ResourceManager/Group/"+GID+"/Attribute",data)
+      return $http.put("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Group/"+GID+"/Attribute",data)
         .then(function (response) {
 
           return response;
@@ -145,12 +143,10 @@
 
     };
 
-
-
     var RemoveAttributeFromGroup = function (attributeId,groupId) {
 
 
-      return $http.delete("http://localhost:8832/DVP/API/6.0//ResourceManager/Group/"+groupId+"/Attribute/"+attributeId)
+      return $http.delete("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0//ResourceManager/Group/"+groupId+"/Attribute/"+attributeId)
         .then(function (response) {
 
           return response;
@@ -162,7 +158,7 @@
     var GetAttributesOfGroup = function (GrpID) {
 
 
-      return $http.get("http://localhost:8832/DVP/API/6.0/ResourceManager/Group/"+GrpID+"/Attribute/Details")
+      return $http.get("http://resourceservice.104.131.67.21.xip.io/DVP/API/1.0.0.0/ResourceManager/Group/"+GrpID+"/Attribute/Details")
         .then(function (response) {
           return response;
         });
--- DVP-Conference/scripts/services/sipUserService.js
+++ DVP-Conference/scripts/services/sipUserService.js
@@ -1,3 +1,37 @@
 /**
  * Created by a on 1/11/2016.
  */
+
+var taskModule = angular.module("sipUserService", []);
+
+taskModule.factory("sipuser", function($http){
+
+  var getExtensions = function(){
+
+    return $http.get("http://127.0.0.1:8086/DVP/API/6.0/SipUser/ExtensionsByCategory/Conference").then(function(response){
+
+
+      if(response.data && response.data.IsSuccess) {
+
+        return response.data.Result;
+
+
+      }else{
+
+        return {};
+      }
+
+
+    });
+  }
+
+
+  return{
+
+    GetExtensions : getExtensions
+
+
+  }
+
+
+});
