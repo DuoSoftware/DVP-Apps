c6c36f5ba97b75a1b1f48c1bb683577570d6a860
waruna <waruna@duosoftware.com>
2016-01-16 02:25:52 +0530

    re-modify application content according to app list

--- DVP-CallServerApp/scripts/controllers/clusterController.js
+++ DVP-CallServerApp/scripts/controllers/clusterController.js
@@ -30,7 +30,7 @@ app.controller("CallListController", function ($scope, $routeParams, $mdDialog, 
   };
 
   $scope.query = {
-    order:"Name",
+    order: "Name",
     limit: 10,
     page: 1
   };
@@ -85,7 +85,7 @@ app.controller("CallListController", function ($scope, $routeParams, $mdDialog, 
 app.controller("CallEditController", function ($scope, $routeParams, $mdDialog, $mdMedia, $location, $log, clusterService) {
 
   $scope.callServer = {};
-
+  $scope.IpAddress = {};
 
   $scope.codes = [
     {Code: 1, name: 'Basic'},
@@ -120,7 +120,7 @@ app.controller("CallEditController", function ($scope, $routeParams, $mdDialog, 
     });
   };
 
-  $scope.updateResource = function () {
+  $scope.updateCallServer = function () {
 
     clusterService.UpdateCallServer($scope.callServer).then(function (response) {
       if (response) {
@@ -134,22 +134,255 @@ app.controller("CallEditController", function ($scope, $routeParams, $mdDialog, 
     });
   };
 
-  $scope.loadResource = function () {
-    clusterService.GetCallServer($routeParams.id).then(function (response) {
-      $scope.callServer = response;
-      clusterService.CallServer = $scope.callServer;
+  $scope.loadCallServer = function () {
+    /*clusterService.GetIpAddresses().then(function (response) {
+      $scope.ipAddress = response;
+    }, function (error) {
+      $scope.showAlert("Error", "Error", "ok", "Fail to get IP Address List.");
+    });*/
+
+    if ($routeParams.id) {
+      clusterService.GetCallServer($routeParams.id).then(function (response) {
+        $scope.callServer = response;
+        clusterService.CallServer = $scope.callServer;
+      }, function (error) {
+        $scope.showAlert("Error", "Error", "ok", "There is an error ");
+      });
+    }
+
+  };
+
+
+  $scope.showAdvanced = function (ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/addIpAddress.html',
+      parent: angular.element(document.body),
+      clickOutsideToClose: false,
+      fullscreen: useFullScreen,
+    })
+      .then(function (answer) {
+        if(answer.IsSuccess)
+          $scope.updateCallServer();
+      }, function () {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    $scope.$watch(function () {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function (wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+  };
+
+
+  function DialogController($scope, $mdDialog, clusterService) {
+    $scope.callServer = clusterService.CallServer;
+
+    $scope.showAlert = function (tittle, label, button, content) {
+
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title(tittle)
+          .textContent(content)
+          .ok(button)
+      );
+    };
+
+    $scope.addIpAddress = function (data) {
+
+      data.CallserverID = clusterService.CallServer.id;
+      clusterService.CreateIpAddress(data).then(function (response) {
+        data.IsSuccess = response;
+        if (response) {
+          $mdDialog.hide(data);
+          clusterService.CallServer.InternalMainIP = data.IP;
+          $scope.showAlert("IP Address", "Add IP Address", "ok", "IP address Successfully Added.");
+        }
+        else {
+          $mdDialog.cancel();
+          $scope.callServer.InternalMainIP = "Error";
+
+          $scope.showAlert("Error", "Error", "ok", "There is an error ");
+        }
+
+      }, function (error) {
+        $scope.callServer.InternalMainIP = "Error";
+        $mdDialog.cancel();
+        $scope.showAlert("Error", "Error", "ok", "There is an error ");
+
+      });
+
+
+    };
+  }
+
+
+  angular.element(document).ready(function () {
+    $scope.loadCallServer();
+  });
+
+
+});
+
+app.controller("ProfileListController", function ($scope, $routeParams, $mdDialog, $mdMedia, $location, $log, clusterService) {
+
+  $scope.showConfirm = function (tittle, label, okbutton, cancelbutton, content, OkCallback, CancelCallBack, okObj) {
+
+
+    var confirm = $mdDialog.confirm()
+      .title(tittle)
+      .textContent(content)
+      .ok(okbutton)
+      .cancel(cancelbutton);
+    $mdDialog.show(confirm).then(function () {
+      OkCallback(okObj);
+    }, function () {
+      CancelCallBack();
+    });
+  };
+
+  $scope.showAlert = function (tittle, label, button, content) {
+
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title(tittle)
+        .textContent(content)
+        .ok(button)
+    );
+  };
+
+  $scope.query = {
+    order: "ProfileName",
+    limit: 10,
+    page: 1
+  };
+  $scope.dataReady = false;
+
+  $scope.loadProfiles = function () {
+
+    clusterService.GetProfiles().then(function (response) {
+
+      $log.debug("GetCallServers: response" + response);
+      $scope.dataReady = true;
+      $scope.profiles = response;
+      $scope.total = response.length;
+
+    }, function (error) {
+      $log.debug("GetCallServers err");
+      $scope.showAlert("Error", "Error", "ok", "There is an error ");
+    });
+
+  };
+
+  $scope.editProfile = function (profileId) {
+
+    $location.path('/profile/' + profileId + '/edit');
+
+  };
+
+  $scope.deleteProfile = function (profile) {
+
+    $scope.showConfirm("Delete Cluster", "Delete", "ok", "cancel", "Do you want to delete " + profile.ProfileName, function (obj) {
+
+      clusterService.DeleteProfile(profile).then(function (response) {
+        if (response) {
+          $scope.loadProfiles();
+          $scope.showAlert("Deleted", "Deleted", "ok", "Call Server " + obj.ProfileName + " Deleted successfully");
+        }
+        else
+          $scope.showAlert("Error", "Error", "ok", "There is an error ");
+      }, function (error) {
+        $scope.showAlert("Error", "Error", "ok", "There is an error ");
+      });
+
+    }, function () {
+
+    }, profile)
+  };
+
+  $scope.loadProfiles();
+
+});
+
+app.controller("ProfileEditController", function ($scope, $routeParams, $mdDialog, $mdMedia, $location, $log, clusterService) {
+
+  $scope.profile = {};
+
+
+  $scope.showAlert = function (tittle, label, button, content) {
+
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title(tittle)
+        .textContent(content)
+        .ok(button)
+    );
+  };
+
+  $scope.createProfile = function () {
+
+    clusterService.CreateProfile($scope.profile).then(function (response) {
+      if (response) {
+        $scope.showAlert("Profile Created", "Profile Created", "ok", "Profile - "+ $scope.profile.ProfileName+" created successfully " );
+        $location.path('/profile/list');
+      } else {
+        $scope.showAlert("Error", "Error", "ok", "There is an error ");
+      }
+    }, function (error) {
+      $log.debug("onGetSuccess3");
+      $scope.showAlert("Error", "Error", "ok", "There is an error ");
+    });
+  };
+
+  $scope.updateProfile = function () {
+
+    clusterService.UpdateProfile($scope.profile).then(function (response) {
+      if (response) {
+        $scope.showAlert("Profile Updated", "Profile Updated", "ok", "Profile - "+ $scope.profile.ProfileName+" Updated successfully." );
+      }
+      else
+        $scope.showAlert("Error", "Error", "ok", "There is an error ");
+
     }, function (error) {
       $scope.showAlert("Error", "Error", "ok", "There is an error ");
     });
   };
 
-  angular.element(document).ready(function () {
-    $scope.loadResource();
-  });
+  $scope.loadProfile = function () {
+
+    clusterService.GetIpAddresses().then(function (response) {
+      $scope.ipAddress = response;
+    }, function (error) {
+      $scope.showAlert("Error", "Error", "ok", "Fail to get IP Address List.");
+    });
+
+    if ($routeParams.id) {
+      clusterService.GetProfile($routeParams.id).then(function (response) {
+        $scope.profile = response;
+        clusterService.Profile = $scope.profile;
+
+      }, function (error) {
+        $scope.showAlert("Error", "Error", "ok", "There is an error ");
+      });
+    }
+
+  };
+
+
+  $scope.loadProfile();
 
 });
 
-app.controller("ClusterConfigController", function ($scope, $routeParams, $mdDialog, $mdMedia, $location, $log, clusterService) {
+app.controller("IPAddressController", function ($scope, $routeParams, $mdDialog, $mdMedia, $location, $log, clusterService) {
+
+
+
 });
-
-
---
+++ DVP-CallServerApp/scripts/services/callServerService.js
@@ -0,0 +1,260 @@
+/**
+ * Created by Rajinda on 12/31/2015.
+ */
+
+var clusterModule = angular.module("clusterServiceModule", []);
+
+clusterModule.factory("clusterService", function ($http, $log) {
+
+  //////////////////************** call server **************/////////////////
+  var getCallServer = function (id) {
+    return $http.get("http://localhost:3636/DVP/API/6.0/CloudConfiguration/CallServer/" + id).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.Result;
+      } else {
+        return {};
+      }
+    });
+  };
+
+  var getCallServers = function () {
+    return $http.get("http://localhost:3636/DVP/API/6.0/CloudConfiguration/CallServers").then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.Result;
+      } else {
+        return undefined;
+      }
+    });
+  };
+
+  var createCallServer = function (callServer) {
+
+    return $http.post("http://localhost:3636/DVP/API/6.0/CloudConfiguration/CallServer", callServer).then(function (response) {
+
+      if (response.data && response.data.IsSuccess) {
+        return response.data.IsSuccess;
+      }
+      else
+        return false;
+    });
+
+  };
+
+  var updateCallServer = function (callServer) {
+    return $http({
+      method: 'put',
+      url: 'http://localhost:3636/DVP/API/6.0/CloudConfiguration/CallServer/' + callServer.id,
+      headers: {
+        'authorization': '1#1'
+      },
+      data: callServer
+    }).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.IsSuccess;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  var deleteCallServer = function (callServer) {
+    return $http.post("http://localhost:3636/DVP/API/6.0/CloudConfiguration/CallServer/" + callServer.id + "/Activate/false", callServer).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.IsSuccess;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  //////////////////************** Cluster Configurations **************/////////////////
+
+  var assignCallServerToCluster = function (callServerId, cloudId) {
+    return $http.post("http://localhost:3636/DVP/API/6.0/CloudConfiguration/CallServer/" + callServerId + "/AssignTo/" + cloudId, {}).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.IsSuccess;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  var deleteCallServerFromCluster = function (cloudId, callServerId) {
+    return $http.delete("http://localhost:3636/DVP/API/:version/CloudConfiguration/CallServer/" + callServerId + "/AssignTo/" + cloudId).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.IsSuccess;
+      } else {
+        return false;
+      }
+    });
+  };
+
+//////////////////************** Network Configurations **************/////////////////
+
+  var getNetworks = function () {
+
+    return $http.get("http://localhost:3636/DVP/API/6.0/CloudConfiguration/Networks").then(function (response) {
+
+      if (response.data && response.data.IsSuccess) {
+
+        return response.data.Result;
+
+
+      } else {
+
+        return {};
+      }
+
+
+    });
+  };
+
+  var assignNetworkToCluster = function (networkId, cloudId) {
+    return $http.post("http://localhost:3636/DVP/API/6.0/CloudConfiguration/Network/" + networkId + "/SetTelcoNetworkToCloud/" + cloudId, {}).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.IsSuccess;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  var deleteNetworkFromCluster = function (cloudId, networkId) {
+    return $http.delete("http://localhost:3636/DVP/API/6.0/CloudConfiguration/Network/" + networkId + "/SetTelcoNetworkToCloud/" + cloudId).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.IsSuccess;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  //////////////////************** Profile **************/////////////////
+
+  var getProfile = function (profileId) {
+
+    return $http.get("http://localhost:3636/DVP/API/6.0/CloudConfiguration/Profile/" + profileId).then(function (response) {
+      if (response.data && response.data.IsSuccess) {
+        return response.data.Result;
+      } else {
+        return {};
+      }
+    });
+  };
+
+  var getProfiles = function () {
+
+    return $http.get("http://localhost:3636/DVP/API/6.0/CloudConfiguration/Profiles").then(function (response) {
+
+      if (response.data && response.data.IsSuccess) {
+
+        return response.data.Result;
+
+
+      } else {
+
+        return {};
+      }
+
+
+    });
+  };
+
+  var updateProfile = function (profile) {
+    return $http({
+      method: 'put',
+      url: 'http://localhost:3636/DVP/API/6.0/CloudConfiguration/Profile/' + profile.id,
+      headers: {
+        'authorization': '1#1'
+      },
+      data: profile
+    }).then(function (response) {
+      return response.data.IsSuccess;
+    });
+  };
+
+  var createProfile = function (profile) {
+    return $http({
+      method: 'post',
+      url: 'http://localhost:3636/DVP/API/6.0/CloudConfiguration/Profile',
+      headers: {
+        'authorization': '1#1'
+      },
+      data: profile
+    }).then(function (response) {
+      return response.data.IsSuccess;
+    });
+  };
+
+  var deleteProfile = function (profile) {
+    return $http({
+      method: 'delete',
+      url: 'http://localhost:3636/DVP/API/6.0/CloudConfiguration/Profile/' + profile.id,
+      headers: {
+        'authorization': '1#1'
+      }
+    }).then(function (response) {
+      return response.data.IsSuccess;
+    });
+  };
+
+  //////////////////************** IPAddresses **************/////////////////
+  var createIpAddress = function (profile) {
+    return $http({
+      method: 'post',
+      url: 'http://localhost:3636/DVP/API/6.0/CloudConfiguration/IPAddress',
+      headers: {
+        'authorization': '1#1'
+      },
+      data: profile
+    }).then(function (response) {
+      return response.data.IsSuccess;
+    });
+  };
+
+  var getIpAddresses = function () {
+
+    return $http.get("http://localhost:3636/DVP/API/6.0/CloudConfiguration/IPAddresses").then(function (response) {
+
+      if (response.data && response.data.IsSuccess) {
+
+        return response.data.Result;
+
+
+      } else {
+
+        return {};
+      }
+
+
+    });
+  };
+
+
+  return {
+
+    CreateIpAddress:createIpAddress,
+    GetIpAddresses:getIpAddresses,
+    IpAddress:{},
+    GetProfile: getProfile,
+    GetProfiles: getProfiles,
+    CreateProfile: createProfile,
+    UpdateProfile: updateProfile,
+    DeleteProfile: deleteProfile,
+    Profile: {},
+    GetCallServer: getCallServer,
+    GetCallServers: getCallServers,
+    CreateCallServer: createCallServer,
+    UpdateCallServer: updateCallServer,
+    DeleteCallServer: deleteCallServer,
+    CallServer: {},
+    AssignCallServerToCluster: assignCallServerToCluster,
+    DeleteCallServerFromCluster: deleteCallServerFromCluster,
+    GetNetworks: getNetworks,
+    AssignNetworkToCluster: assignNetworkToCluster,
+    DeleteNetworkFromCluster: deleteNetworkFromCluster
+
+
+  }
+
+});
