0ae3dd100ea5e67a30e32eb7b82305f555023cff
heshanindika <k.heshan.indika@gmail.com>
2016-02-01 17:39:29 +0530

    finish DVP-ArdsConfigurationApp

--- DVP-ArdsConfigurationApp/scripts/controllers/updateConfigController.js
+++ DVP-ArdsConfigurationApp/scripts/controllers/updateConfigController.js
@@ -1,3 +1,93 @@
 /**
  * Created by Heshan.i on 1/29/2016.
  */
+(function(){
+  var app = angular.module('dvp-ardsConfigurationApp');
+  var updateConfigController = function($scope, $routeParams, ardsService, resourceService, $mdDialog, $location){
+    var accessToken = "1#1";
+    var onGetOneReqMetaData = function(data){
+      if(data.IsSuccess){
+        $scope.metaData = JSON.parse(data.Result);
+        resourceService.GetTasks(accessToken).then(onGetTasksComplete, function(){onError("fetch", "Tasks")});
+        resourceService.GetGroups(accessToken).then(onGetGroupsComplete, function(){onError("fetch", "Groups")});
+      }else{
+        $scope.showAlert("Error", "OK", data.CustomMessage);
+      }
+    };
+    var onGetTasksComplete = function(data){
+      if(data.IsSuccess){
+        $scope.tasks = data.Result;
+      }else{
+        $scope.showAlert("Error", "OK", data.CustomMessage);
+      }
+    };
+    var onGetGroupsComplete = function(data){
+      if(data.IsSuccess){
+        $scope.groups = data.Result;
+        $scope.attributeGroups = [];
+        for(var i=0; i<$scope.metaData.AttributeGroups.length; i++){
+          var group = $scope.groups.inArray($scope.metaData.AttributeGroups[i]);
+          if(group != null) {
+            $scope.attributeGroups.push(group);
+          }
+        }
+      }else{
+        $scope.showAlert("Error", "OK", data.CustomMessage);
+      }
+    };
+    var onEditReqMetaData = function(data){
+      if(data.IsSuccess){
+        $scope.showAlert("Susscess", "OK", data.CustomMessage);
+        $scope.backToMain();
+      }else{
+        $scope.showAlert("Error", "OK", data.CustomMessage);
+      }
+    };
+    var onError = function(action, message){
+      $scope.showAlert("Error", "OK", "Could not "+action+" the "+message+" data");
+    };
+
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(group) {
+        return (group.GroupName.toLowerCase().indexOf(lowercaseQuery) != -1);;
+      };
+    }
+    Array.prototype.inArray = function(comparer) {
+      for(var i=0; i < this.length; i++) {
+        if(comparer === (this[i].GroupId)) return this[i];
+      }
+      return null;
+    };
+
+    $scope.querySearch = function(query) {
+      var results = query ? $scope.groups.filter(createFilterFor(query)) : [];
+      return results;
+    };
+    $scope.showAlert = function(tittle, button, content) {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title(tittle)
+          .textContent(content)
+          .ok(button)
+      );
+    };
+    $scope.updateMetaData = function(){
+      $scope.metaData.AttributeGroups = [];
+      for(var i=0; i< $scope.attributeGroups.length; i++){
+        $scope.metaData.AttributeGroups.push($scope.attributeGroups[i].GroupId);
+      }
+      ardsService.editReqMetaData(accessToken, $scope.metaData).then(onEditReqMetaData, function(){onError("update", "Meta")});
+    };
+    $scope.backToMain = function(){
+      $location.url("/ards/configuration");
+    };
+    $scope.loadData = function(){
+      ardsService.getOneReqMetaData(accessToken,$routeParams.serverType, $routeParams.requestType).then(onGetOneReqMetaData, function(){onError("fetch", "Meta")});
+    };
+    $scope.loadData();
+  };
+  app.controller('updateConfigController', updateConfigController);
+}());
--- DVP-ArdsConfigurationApp/scripts/controllers/viewConfigController.js
+++ DVP-ArdsConfigurationApp/scripts/controllers/viewConfigController.js
@@ -1,3 +1,67 @@
 /**
  * Created by Heshan.i on 1/29/2016.
  */
+(function(){
+  var app = angular.module('dvp-ardsConfigurationApp');
+  var viewConfigController = function($scope, ardsService, $location, $mdDialog){
+    var accessToken = "1#1";
+    var onGetReqMetaDataComplete = function(data){
+      if(data.IsSuccess){
+        $scope.reqMetaData = data.Result;
+        $scope.total = $scope.reqMetaData.length;
+      }else{
+        $scope.showAlert("Error", "OK", data.CustomMessage);
+      }
+    };
+    var onDeleteReqMetaData = function(data){
+      if(data.IsSuccess){
+        $scope.showAlert("Success", "OK", "Delete ARDS configuration success");
+        $scope.loadData();
+      }else{
+        $scope.showAlert("Error", "OK", data.CustomMessage);
+      }
+    };
+    var onError = function(){
+      $scope.showAlert("Error", "OK", "Could not fetch the configuration data");
+    };
+
+    $scope.query = {
+      limit: 5,
+      page: 1
+    };
+    $scope.showAlert = function(tittle, button, content) {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title(tittle)
+          .textContent(content)
+          .ok(button)
+      );
+    };
+    $scope.deleteMetaData = function(ev, serverType, requestType) {
+      var confirm = $mdDialog.confirm()
+        .title('Delete Configuration')
+        .textContent('Would you like to delete ARDS configuration?')
+        .ariaLabel('')
+        .targetEvent(ev)
+        .ok('Delete')
+        .cancel('Cancel');
+      $mdDialog.show(confirm).then(function() {
+        ardsService.deleteReqMetaData(accessToken,serverType,requestType).then(onDeleteReqMetaData, onError);
+      }, function() {
+      });
+    };
+    $scope.addMetaData = function(){
+      $location.url("/ards/configuration/add");
+    };
+    $scope.editMetaData = function(serverType, requestType){
+      $location.url("/ards/configuration/edit/"+serverType+"/"+requestType);
+    };
+    $scope.loadData = function(){
+      ardsService.getReqMetaData(accessToken).then(onGetReqMetaDataComplete, onError);
+    };
+    $scope.loadData();
+  };
+  app.controller("viewConfigController", viewConfigController);
+}());
