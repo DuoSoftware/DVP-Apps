a341375ec09c9e4edd2ec13b82b5554f16348ccc
achinthau <achintha@duosoftware.com>
2016-02-03 17:22:12 +0530

    update CloudeUrls

--- HoldMusicApp/scripts/services/BackendController.js
+++ HoldMusicApp/scripts/services/BackendController.js
@@ -12,7 +12,7 @@
 
     var getHoldMusicList = function () {
       console.log("BACKENDCONTROLLER");
-      return $http.get("http://127.0.0.1:9093/DVP/API/1.0.0.0/QueueMusic/Profiles")
+      return $http.get("http://queuemusic.104.131.67.21.xip.io/DVP/API/1.0.0.0/QueueMusic/Profiles")
         // return $http.get("http://queuemusic.104.131.67.21.xip.io/DVP/API/1.0.0.0/QueueMusic/Profiles")
         .then(function (response) {
           console.log("BACKENDCONTROLLER2  -"+ JSON.stringify(response));
@@ -26,7 +26,7 @@
       console.log("deleteMusicProfile-- "+ name);
       // return $http.post("http://appregistry.104.131.67.21.xip.io/DVP/API/1.0.0.0/APPRegistry/Application/"+AppId+"/Activate/false")
       // return $http.post("http://192.168.0.88:8013/DVP/API/6.0/APPRegistry/Application/"+AppId+"/Activate/false")
-      return $http.delete("http://127.0.0.1:9093/DVP/API/1.0.0.0/QueueMusic/Profile/"+name)
+      return $http.delete("http://queuemusic.104.131.67.21.xip.io/DVP/API/1.0.0.0/QueueMusic/Profile/"+name)
         ///
         // ('/DVP/API/:version/QueueMusic/Profile/:name'
         .then(function (response) {
@@ -52,7 +52,7 @@
       console.log(Attribute.Name);
       console.log(Attribute);
       // return $http.put("http://192.168.0.88:8013/DVP/API/6.0/APPRegistry/Application/"+Attribute.id,Attribute)
-      return $http.put("http://127.0.0.1:9093/DVP/API/1.0.0.0/QueueMusic/Profile/"+Attribute.Name,Attribute)
+      return $http.put("http://queuemusic.104.131.67.21.xip.io/DVP/API/1.0.0.0/QueueMusic/Profile/"+Attribute.Name,Attribute)
         //QueueMusic/Profile/
         .then(function (response) {
 
@@ -77,7 +77,7 @@
       console.log("createNewMusicProfile");
       console.log("AAAAAAAAAAAA---"+JSON.stringify(NewAppDataObj));
 
-      return $http.post("http://127.0.0.1:9093/DVP/API/1.0.0.0/QueueMusic/Profile",NewAppDataObj)
+      return $http.post("http://queuemusic.104.131.67.21.xip.io/DVP/API/1.0.0.0/QueueMusic/Profile",NewAppDataObj)
         .then(function (response) {
           if (response.data && response.data.IsSuccess) {
 
